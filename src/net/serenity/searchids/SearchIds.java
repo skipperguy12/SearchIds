597fd46c5e489987ee79048b94ec0adf5a6f3e75
Tony <tony@bzextreme.com>
2011-03-07 09:27:11 -0600

    Remove Player Listener for onCommand

--- src/net/serenity/searchids/SearchIds.java
+++ src/net/serenity/searchids/SearchIds.java
@@ -59,8 +59,6 @@ public class SearchIds extends JavaPlugin  {
 
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Priority.Normal, this);
-
 		log.info(name + " " + version + " enabled");
 
 		if (!initProps()) {
@@ -127,6 +125,28 @@ public class SearchIds extends JavaPlugin  {
 			return false;
 		}
 		return true;
+	}
+	
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String args[])
+    {
+    	String[] split = command.split(" ");
+        Player player = (Player)sender;
+		String command = command;
+	
+		if((!event.isCancelled()) && command.equalsIgnoreCase("/" + SearchIds.searchCommand)) {
+			if (split.length > 1) {
+				String query = "";
+				for (int i = 1; i<split.length; i++) {
+					query += (split[i] + " ");
+				}
+				query = query.trim();
+				plugin.printSearchResults(player, SearchIds.parser.search(query, SearchIds.base), query);
+			} else {
+				player.sendMessage(ChatColor.RED + "Correct usage is: " + "/"+SearchIds.searchCommand + " [item to search for]");
+			}
+			
+			event.setCancelled(true);
+		}
 	}
 	
 	public boolean updateData() {
